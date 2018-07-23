d67576e4625cb4aa985c0e2af654aa860d394f21
Pavel Sergeyev <xblxTurtle@gmail.com>
2018-07-23 10:48:26 +0100

    Added port configuration for Heroku

--- server/server.js
+++ server/server.js
@@ -1,12 +1,13 @@
 const path = require('path');
 const express = require('express');
 const publicPath = path.join(__dirname+'/../public');
-
 const app = express();
+
+const port = process.env.PORT || 3000;
 
 //console.log(__dirname+'/../public');
 //console.log(publicPath);
 app.use(express.static(publicPath));
-app.listen(3000, ()=> {
+app.listen(port, ()=> {
   console.log('server is listening port 3000');
 });
